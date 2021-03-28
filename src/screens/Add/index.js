6f02615c9847e750f2f82ac28f0ecb12f77f86f1
Sofia Reis <sofiareis2018@gmail.com>
2021-03-28 11:17:20 -0700

    new trip

--- src/screens/Add/index.js
+++ src/screens/Add/index.js
@@ -12,13 +12,45 @@ import { Icon, InlineIcon } from '@iconify/react';
 
 
 
-function Add(){
- 
-  const handleClick = () => window.location.href = "/home";
+class Add extends React.Component{
 
-  const [startDate, setStartDate] = useState(new Date());
+  constructor(props){
+    super(props);
+    this.state = {
+      trip_name: '',
+      date: new Date(),
+      trip_start: '',
+      trip_destination: '',
+    };
 
-  const ColoredLine2 = ({ color }) => (
+    this.update_trip_name = this.update_trip_name.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.update_trip_destination = this.update_trip_destination.bind(this);
+    this.update_trip_start = this.update_trip_start.bind(this);
+
+  }
+  
+  // Update the state with the task name given by the user
+  async update_trip_name(event) {
+    await this.setState({ trip_name: event.target.value});
+  }
+
+  async update_trip_destination(event) {
+    await this.setState({ trip_destination: event.target.value});
+  }
+
+  async update_trip_start(event) {
+    await this.setState({ trip_start: event.target.value});
+  }
+
+  handleChange = date => {
+    this.setState({
+      date: date
+    });
+  };
+
+
+  ColoredLine2 = ({ color }) => (
     <hr
         style={{
             color: color,
@@ -29,25 +61,31 @@ function Add(){
   );
 
 
-
+render(){
   return (
     <div className="content">
-      <div id="cancel" onClick={handleClick}>
+      <div id="cancel"onClick={()=>{window.location.href = "/home"}}>
         <p style={{marginLeft: "10px", color: "#3A72B4"}}>Cancel</p>
       </div>
-      <div id="header">
+      <div style ={{margin:'auto', textAlign: 'center'}} id="header">
         <h1>New Trip</h1>
       </div>
       <div id="add-container">
         <p style={{marginLeft: "10px"}}><strong>Trip Name*</strong></p>
-        <div className="rectangleAdd"></div>
+        <div className="rectangleAdd">
+        <input type="text" placeholder="Enter trip name" onChange={this.update_trip_name} />
+        </div>
         <p style={{marginLeft: "10px"}}><strong>Start Location*</strong></p>
-        <div className="rectangleAdd"></div>
+        <div className="rectangleAdd">
+        <input type="text" placeholder="Enter start location" onChange={this.update_trip_start} />
+        </div>
         <p style={{marginLeft: "10px"}}><strong>Destiantion Location*</strong></p>
-        <div className="rectangleAdd"></div>
+        <div className="rectangleAdd">
+        <input type="text" placeholder="Enter destination location" onChange={this.update_trip_destination} />
+        </div>
         <p style={{marginLeft: "10px"}}><strong>Departure Date*</strong></p>
         <div className="rectangleDept">
-        <DatePicker selected={startDate} onChange={date => setStartDate(date)}/>
+        <DatePicker selected={this.state.date} onChange={this.handleChange}/>
         </div>
         <Icon icon={calendarIcon} style={{fontSize: '30px'}} ></Icon>
       </div>
@@ -59,6 +97,7 @@ function Add(){
     </div>
 
   );
+  }
 }
 
 export default Add;
