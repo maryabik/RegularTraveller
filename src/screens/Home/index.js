baaa6fc55779fa0783c8af5d7f23d61bf9e93761
emilyl <emily.lukas@gmail.com>
2021-03-28 13:03:23 -0700

    cleanup a bit of home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -7,46 +7,30 @@ import { UserContext } from '../../services/UserProvider';
 import { Icon, InlineIcon } from '@iconify/react';
 import copy24Filled from '@iconify/icons-fluent/copy-24-filled';
 import outlineTrain from '@iconify/icons-ic/outline-train';
-
-
-
-
-
-
+import TorontoPic from '../../assets/TorontoPic.png';
 
 function Home(){
   const { logout } = useContext(UserContext);
 
   return (
-    <div className="content">
-      <div id="header">
-        <h1>My Trips</h1>
-      </div>
-      <div id="home-container">
-       <div className="rectangle">
-         <div className="rectangleHeader">
-           <p><strong>Vancouver to Toronto</strong></p>
-           Thursday, April 6 2021
-         </div>
-        <div className="rectangleContainer">
-          <div className="icom1"> 
-          <Icon icon={outlineTrain} style={{color: '#989898', fontSize: '25px'}}  />
-           <Icon icon={copy24Filled} style={{color: '#686868', fontSize: '35px'}} onClick={()=>{window.location.href = "/duplicate"}} />
+    <div className="homeContent">
+      <h1 className="homeTitle">My Trips</h1>
+      <div className="tripItem">
+        <img src={TorontoPic} />
+        <div className="tripItemContent">
+          <p className="tripItemTitle"><strong>Vancouver to Toronto</strong></p>
+          <p className="tripItemTitle">Thursday, April 6 2021</p>
+          <div className="tripItemIconRow">
+            <div className="icom1">
+              <Icon icon={outlineTrain} style={{color: '#989898', fontSize: '25px'}}  />
+              <Icon icon={copy24Filled} style={{color: '#686868', fontSize: '35px'}} onClick={()=>{window.location.href = "/duplicate"}} />
+            </div>
           </div>
-    
         </div>
-       </div>
       </div>
-
-    
-    <button className="buttonPlus"  onClick={() => window.location.href = "/addnew"}>+</button>
-      
-    <div className="signoutBtn" onClick={() => logout()}>LOG OUT</div>
+      <button className="buttonPlus"  onClick={() => window.location.href = "/addnew"}>+</button>
+      <div className="signoutBtn" onClick={() => logout()}>LOG OUT</div>
     </div>
-
-
-    
-
   );
 }
 
